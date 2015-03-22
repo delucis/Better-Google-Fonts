652288575584bc4541ed48ac0d1a05e5fcef5bb3
delucis <swithinbank@gmail.com>
2015-03-21 21:36:12 -0400

    Added function to get URL query variables
    HT: https://css-tricks.com/snippets/javascript/get-url-variables/

--- google-families.js
+++ google-families.js
@@ -81,6 +81,17 @@
 		$("#fonts").html(fontsView.el);
 	}
 
+	// gets query variables from URL
+	function getQueryVariable(variable){
+		var query = window.location.search.substring(1);
+		var vars = query.split("&");
+		for (var i=0;i<vars.length;i++) {
+						var pair = vars[i].split("=");
+						if(pair[0] == variable){return pair[1];}
+		}
+		return(false);
+	}
+
 	jQuery(document).ready(function() {
 		$.ajax(api, {
 			dataType: 'jsonp',
